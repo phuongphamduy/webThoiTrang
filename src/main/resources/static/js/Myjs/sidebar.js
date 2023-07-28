a33e1c25fb2266f2f403832e9ff6bc6124a797a0
tuanleduc <tuanleduc999@gmail.com>
2023-07-29 00:15:49 +0700

    admin

---
+++ src/main/resources/static/js/Myjs/sidebar.js
@@ -0,0 +1,56 @@
+function myFunction1() {
+  var x1 = document.getElementById("Demo1");
+  if (x1.className.indexOf("w3-show") == -1) {
+    x1.className += " w3-show";
+  } else {
+    x1.className = x1.className.replace(" w3-show", "");
+  }
+}
+function myFunction2() {
+  var x2 = document.getElementById("Demo2");
+  if (x2.className.indexOf("w3-show") == -1) {
+    x2.className += " w3-show";
+  } else {
+    x2.className = x2.className.replace(" w3-show", "");
+  }
+}
+function myFunction3() {
+  var x3 = document.getElementById("Demo3");
+  if (x3.className.indexOf("w3-show") == -1) {
+    x3.className += " w3-show";
+  } else {
+    x3.className = x3.className.replace(" w3-show", "");
+  }
+}
+function myFunction4() {
+  var x4 = document.getElementById("Demo4");
+  if (x4.className.indexOf("w3-show") == -1) {
+    x4.className += " w3-show";
+  } else {
+    x4.className = x4.className.replace(" w3-show", "");
+  }
+}
+function myFunction5() {
+  var x5 = document.getElementById("Demo5");
+  if (x5.className.indexOf("w3-show") == -1) {
+    x5.className += " w3-show";
+  } else {
+    x5.className = x5.className.replace(" w3-show", "");
+  }
+}
+function myFunction6() {
+  var x6 = document.getElementById("Demo6");
+  if (x6.className.indexOf("w3-show") == -1) {
+    x6.className += " w3-show";
+  } else {
+    x6.className = x6.className.replace(" w3-show", "");
+  }
+}
+function myFunction7() {
+  var x7 = document.getElementById("Demo7");
+  if (x7.className.indexOf("w3-show") == -1) {
+    x7.className += " w3-show";
+  } else {
+    x7.className = x7.className.replace(" w3-show", "");
+  }
+}
