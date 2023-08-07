8ae32c3c4a7d95a406e310f0b2ebc64e845abb6f
tranquoctri1933 <tranquoctri1933@gmail.com>
2023-08-08 00:53:32 +0700

    order

--- src/main/java/com/example/dao/OrderDAO.java
+++ src/main/java/com/example/dao/OrderDAO.java
@@ -1,9 +1,14 @@
 package com.example.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.entity.Order;
 
 public interface OrderDAO extends JpaRepository<Order, Integer>{
-
+	
+	@Query("select o from Order o where o.account.username = ?1")
+	List<Order> findByUsername(String username);
 }
