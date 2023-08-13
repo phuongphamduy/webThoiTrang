5b0140a4024a9908e562e9d33bd38e62d8d330bf
Nguyễn Bảo Trọng <110366231+Ngaicugugu@users.noreply.github.com>
2023-08-13 22:19:07 +0700

    UserChange

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -6,11 +6,13 @@ import com.example.entity.Account;
 
 public interface AccountService {
 	List<Account> getUser();
-	
-	Account findById(String id, String username);
 
 	Account findById(String id);
 	
 	Account FindByUsernameActivated(String id);
+
+	Account update(Account account);
+
+	
 	
 }
